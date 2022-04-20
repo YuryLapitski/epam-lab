fe7217a8ac8686eb3e46820f431fdf63d14584cb
YuryLapitski <yura.lapitski@gmail.com>
2022-04-20 20:21:10 +0300

    Create Java docs

--- module2/service/src/main/java/com/epam/esm/service/TagToGiftCertificateService.java
+++ module2/service/src/main/java/com/epam/esm/service/TagToGiftCertificateService.java
@@ -4,8 +4,28 @@ import com.epam.esm.entity.TagToGiftCertificateRelation;
 import com.epam.esm.service.dto.GiftCertificateDto;
 import java.util.List;
 
+/**
+ * The TagToGiftCertificateService interface provides methods for creating
+ * and searching tag to gift certificate relation.
+ *
+ * @author Yury Lapitski
+ * @version 1.0
+ */
 public interface TagToGiftCertificateService {
-    TagToGiftCertificateRelation createTagToGiftCertificateRelation(long giftCertificateId, long tagId);
 
+    /**
+     * Creates new tag to gift certificate relation
+     *
+     * @param tagId tag to create
+     * @return tag to gift certificate relation
+     */
+    TagToGiftCertificateRelation createTagToGiftCertificateRelation(long tagId, long giftCertificateId);
+
+    /**
+     * Searches for gift certificates by tag name
+     *
+     * @param tagName name of the tag to find gift certificates
+     * @return founded tag
+     */
     List<GiftCertificateDto> findGiftCertificatesByTagName(String tagName);
 }
--- module2/service/src/main/java/com/epam/esm/service/validator/TagValidator.java
+++ module2/service/src/main/java/com/epam/esm/service/validator/TagValidator.java
@@ -1,5 +1,18 @@
 package com.epam.esm.service.validator;
 
+/**
+ * The TagValidator interface provides method for tag name validation,
+ *
+ * @author Yury Lapitski
+ * @version 1.0
+ */
 public interface TagValidator {
+
+    /**
+     * Validates entered tag name
+     *
+     * @param name tag name to validate
+     * @return true if the tag name is valid
+     */
     boolean isNameValid(String name);
 }
